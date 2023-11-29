03a78d43ab64e54004d311a5fe62e7451f3bea80
Anshal <anshalantony10@gmail.com>
2023-11-29 01:43:11 +0000

    sqlite

--- server.py
+++ server.py
@@ -2,11 +2,9 @@ from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 import requests
 
-app = Flask(__name__, template_folder="template")
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@db/mydatabase'  # Replace with your PostgreSQL connection string
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable Flask-SQLAlchemy modification tracking
-
-db = SQLAlchemy(app)
+app = Flask(__name__,template_folder="template") 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///location.db'
+db = SQLAlchemy(app)  
 
 class Location(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -83,4 +81,4 @@ def get_map():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
